8d8ee10552f845b94c88cce6666d1b50bf57013b
Daniel <46061817+nevernervous78@users.noreply.github.com>
2021-04-27 14:34:54 +1000

    Update rgb_2_lab.py
    RGB linearisation added

--- rgb_2_lab.py
+++ rgb_2_lab.py
@@ -1,4 +1,4 @@
-__author__ = "Daniele (Daniel) Pelliccia"
+__author__ = "Daniele Pelliccia"
 __copyright__ = "Copyright 2020, Rubens Technologies"
 
 __license__ = "MIT"
@@ -6,35 +6,50 @@ __version__ = "0.0.1"
 
 import numpy as np
 
-def rgb2lab(R,G,B,C):
+def rgb2lab(R,G,B,C, illuminant='D50'):
 
     # Normalise values
     r = R/C
     g = G/C
     b = B/C
 
+    # Linearise RGB values
+    rgb = np.array([r,g,b])
+    for i,j in enumerate(rgb):
+        if j > 0.04045 :
+            rgb[i] = ( ( j + 0.055 ) / 1.055 ) ** 2.4
+        else :
+            rgb[i] = j / 12.92
+
     # Convert to XYZ. Need to decide what RGB we are using
     # http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+    # https://en.wikipedia.org/wiki/CIELAB_color_space
 
-    # sRGB
-    M = np.array([[0.4360747,  0.3850649,  0.1430804], \
-                 [0.2225045,  0.7168786,  0.0606169], \
-                 [0.0139322,  0.0971045,  0.7141733]])
-
-    rgb = np.array([r,g,b])
+    if illuminant == 'D50':
+        # sRGB
+        M = np.array([[0.4360747,  0.3850649,  0.1430804], \
+                     [0.2225045,  0.7168786,  0.0606169], \
+                     [0.0139322,  0.0971045,  0.7141733]])
+        Xn = 96.4242
+        Yn = 100.
+        Zn = 82.5188
+    elif illuminant == 'D65':
+        M = np.array([[0.4124564,  0.3575761,  0.1804375], \
+                      [0.2126729,  0.7151522,  0.0721750], \
+                      [0.0193339,  0.1191920,  0.9503041]])
+        # D65 illuminant
+        Xn = 95.0489
+        Yn = 100.
+        Zn = 108.5188
+    else:
+        print("Error. 'D50' or 'D65' are the only allowed values for illuminant.")
+        return
 
     XYZ = np.dot(M,rgb)*100
 
     X = XYZ[0]
     Y = XYZ[1]
     Z = XYZ[2]
-
-    # Convert to CIE LAB
-    # https://en.wikipedia.org/wiki/CIELAB_color_space
-    # CIE XYZ tristimulus for D50 illuminant
-    Xn = 96.4242
-    Yn = 100.
-    Zn = 82.5188
 
     def f(t):
         delta = 6./29
@@ -50,12 +65,3 @@ def rgb2lab(R,G,B,C):
     b = 200*(f(Y/Yn)-f(Z/Zn))
 
     return (L,a,b)
-
-# Input the raw data reading of the Adafruit TCS34725 RGB Color Sensor Module
-# Example
-R = 18628
-G = 31805
-B = 16424
-C = 65535
-L,a,b = rgb2lab(R,G,B,C)
-print(L, a, b)
